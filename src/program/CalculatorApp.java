d2e06f14e66fc51269be13cc3a5f124012b912ff
Gustavo Andrino <gustavohtandrino@gmail.com>
2024-05-09 11:42:56 -0300

    Basic Gui Elements Added

--- src/program/CalculatorApp.java
+++ src/program/CalculatorApp.java
@@ -5,5 +5,7 @@ import gui.CalculatorGui;
 public class CalculatorApp {
 	public static void main(String[] args) {
 		new CalculatorGui().setVisible(true);
+		
+		System.out.println("Hello Calculator world");
 	}
 }
